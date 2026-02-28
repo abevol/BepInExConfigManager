eb39c15ce425ffc8bfaede10cee5f2e49100840a
abevol <abevol@pm.me>
2026-03-01 01:26:26 +0800

    refactor: replace SnapTo/IsInView with section-based navigation in UIExtensions

--- src_plugin/UI/UIExtensions.cs
+++ src_plugin/UI/UIExtensions.cs
@@ -5,13 +5,13 @@ namespace ConfigManager.UI
 {
     public static class UIExtensions
     {
-        public static void SnapTo(this ScrollRect @this, RectTransform child)
+        public static void SnapToSection(this ScrollRect @this, RectTransform section)
         {
             Canvas.ForceUpdateCanvases();
 
             var contentPos = (Vector2)@this.transform.InverseTransformPoint(@this.content.position);
-            var childPos = (Vector2)@this.transform.InverseTransformPoint(child.position);
-            var endPos = contentPos - childPos - (Vector2)child.rect.size * 0.5f;
+            var childPos = (Vector2)@this.transform.InverseTransformPoint(section.position);
+            var endPos = contentPos - childPos - (Vector2)section.rect.size * 0.5f;
             // If no horizontal scroll, then don't change contentPos.x
             if (!@this.horizontal)
                 endPos.x = @this.content.anchoredPosition.x;
@@ -21,21 +21,18 @@ namespace ConfigManager.UI
             @this.content.anchoredPosition = endPos;
         }
 
-        public static bool IsInView(this ScrollRect @this, RectTransform child)
+        public static bool IsInSection(this ScrollRect @this, RectTransform section)
         {
             var contentPos = (Vector2)@this.transform.InverseTransformPoint(@this.content.position);
-            var childPos = (Vector2)@this.transform.InverseTransformPoint(child.position);
-            var endPos = contentPos - childPos - (Vector2)child.rect.size * 0.5f;
-
-            Vector2 viewportPosition = @this.content.anchoredPosition;
-            Rect viewportRect = new Rect(viewportPosition, @this.viewport.rect.size);
-            Rect childRect = child.rect;
-            Vector2 childPosition = (Vector2)@this.transform.InverseTransformPoint(child.position);
-
-            var childTopLeft = contentPos - childPosition - (Vector2)childRect.size * 0.5f;
-            var childBottomRight = contentPos - childPosition + (Vector2)childRect.size * 0.5f;
-
-            return childTopLeft.y >= viewportRect.yMin && childBottomRight.y <= viewportRect.yMax;
+            var childPos = (Vector2)@this.transform.InverseTransformPoint(section.position);
+            var endPos = contentPos - childPos + (Vector2)section.rect.size * 0.5f;
+            // If horizontal scroll, then compare with contentPos.x
+            if (@this.horizontal)
+                return endPos.x >= @this.content.anchoredPosition.x;
+            // If vertical scroll, then compare with contentPos.y
+            if (@this.vertical)
+                return endPos.y >= @this.content.anchoredPosition.y;
+            return false;
         }
     }
 }
